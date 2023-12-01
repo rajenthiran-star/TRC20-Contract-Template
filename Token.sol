d9191d5b7869d8c3ac51271e12074cea0ea19e78
rajenthiran-star <125804529+rajenthiran-star@users.noreply.github.com>
2023-12-01 13:17:56 +0530

    FCD Token.sol

--- Token.sol
+++ Token.sol
@@ -6,17 +6,17 @@ import "./TRC20.sol";
 import "./TRC20Detailed.sol";
 
 /**
- * @title SimpleToken
+ * @FCDIVINE
  * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `TRC20` functions.
  */
 contract Token is TRC20, TRC20Detailed {
 
-    /**
+    /**@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed 
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor (TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c) public TRC20Detailed("FCDIVINE", "FCD", 18) {
+        _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
     }
 }
